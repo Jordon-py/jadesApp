532b0ba0986f16abb9fbd88356bd49e77930a9a5
Christopher Jordon <c.jordon@icloud.com>
2025-04-07 18:31:02 -0700

    Prepare frontend for Heroku deploy

--- jadesbrowbeauty-vite/vite.config.js
+++ jadesbrowbeauty-vite/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: [ '.js', '.jsx' ],
-    alias: { '.js': 'jsx' } // ✅ changed to key-value pair
+    extensions: ['.js', '.jsx']
   },
   build: {
-    target: 'esnext',
+    target: 'esnext'
   },
+  base: './' // for relative asset paths
 });
