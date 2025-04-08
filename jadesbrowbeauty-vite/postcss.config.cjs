61d90891faebdcc4d9f3653f1a3439b91d2ea240
Christopher Jordon <c.jordon@icloud.com>
2025-04-07 22:19:44 -0700

    Fix PostCSS config for Heroku build

--- jadesbrowbeauty-vite/postcss.config.cjs
+++ jadesbrowbeauty-vite/postcss.config.cjs
@@ -1,6 +1,11 @@
+const postcssImport = require('postcss-import');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    postcssImport(),
+    tailwindcss,
+    autoprefixer,
+  ],
+};
