c89dc43504e85637b132a748e9b6091e68f5e2c6
Christopher Jordon <c.jordon@icloud.com>
2025-04-07 20:36:29 -0700

    sync

--- jadesbrowbeauty-vite/vite.config.js
+++ jadesbrowbeauty-vite/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx']``
   },
   build: {
     target: 'esnext'
